d39975b7ad35ffafa23bce5d1221c69ad5a95df8
Mateusz Żurowski <mateusz.zurowski@gmail.com>
2021-03-18 03:00:53 +0100

    fix

--- mpi.c
+++ mpi.c
@@ -15,18 +15,18 @@ void pingpong(long round_trips, long data_size) {
     if (my_rank == 0) {
         for (int i = 0; i < round_trips; ++i) {
             double start = MPI_Wtime();
-            MPI_Send(&data, data_size, MPI_INT, 1, 0,
+            MPI_Send(data, data_size, MPI_INT, 1, 0,
                      MPI_COMM_WORLD);
-            MPI_Recv(&data, data_size, MPI_INT, 1, 0,
+            MPI_Recv(data, data_size, MPI_INT, 1, 0,
                      MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             double end = MPI_Wtime();
             times[i] = end - start;
         }
     } else {
         for (int i = 0; i < round_trips; ++i) {
-            MPI_Recv(&data, data_size, MPI_INT, 0, 0,
+            MPI_Recv(data, data_size, MPI_INT, 0, 0,
                      MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&data, data_size, MPI_INT, 0, 0,
+            MPI_Send(data, data_size, MPI_INT, 0, 0,
                      MPI_COMM_WORLD);
         }
     }
@@ -38,7 +38,7 @@ void pingpong(long round_trips, long data_size) {
         }
         double average = sum/(double)round_trips;
         printf("Round trips: %ld\n", round_trips);
-        printf("Element size: %d bytes\n", sizeof(int));
+        printf("Element size: %zu bytes\n", sizeof(int));
         printf("Data size: %ld elements\n", data_size);
         printf("Average round trip time: %.10e\n", average);
     }
